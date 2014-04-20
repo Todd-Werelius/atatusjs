b03c435982342cbfb6b371500bd7d96b308f664c
Fizer Khan <fizerkhan@gmail.com>
2014-04-20 13:06:53 +0530

    Updated atatus to support custom data. Added localstorage support.

--- src/atatus.js
+++ src/atatus.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT license.
  */
 
-(function (window, $, UAParser) {
+(function (window, $, UAParser, undefined) {
   // pull local copy of TraceKit to handle stack trace collection
   var _traceKit = TraceKit.noConflict(),
       _atatus = window.atatus,
@@ -14,10 +14,13 @@
       _userAgent = {},
       _debugMode = false,
       _allowInsecureSubmissions = false,
+      _enableOfflineSave = false,
       _customData = {},
+      _tags = [],
       _user,
       _version,
       _allowedDomains,
+      _atatusApiUrl = 'https://www.atatus.com',
       $document;
 
   if ($) {
@@ -47,12 +50,17 @@
         }
       }
 
+      sendSavedErrors();
       return atatus;
     },
 
     withCustomData: function (customdata) {
       _customData = customdata;
       return atatus;
+    },
+
+    withTags: function (tags) {
+      _tags = tags;
     },
 
     attach: function () {
@@ -74,15 +82,18 @@
       return atatus;
     },
 
-    send: function (ex, customData) {
+    send: function (ex, customData, tags) {
       try {
-        processUnhandledException(_traceKit.computeStackTrace(ex), merge(_customData, customData));
-      }
-      catch (traceKitException) {
+        processUnhandledException(_traceKit.computeStackTrace(ex), {
+          customData: merge(_customData, customData),
+          tags: mergeArray(_tags, tags)
+        });
+      } catch (traceKitException) {
         if (ex !== traceKitException) {
           throw traceKitException;
         }
       }
+
       return atatus;
     },
 
@@ -94,7 +105,7 @@
     },
 
     setUser: function (user) {
-      _user = user;
+      _user = { 'Identifier': user };
       return atatus;
     },
 
@@ -103,35 +114,56 @@
       return atatus;
     },
 
-    captureEvent: function(message, tags, data) {
-      if (!message) {
-        return;
+    saveIfOffline: function (enableOffline) {
+      if (typeof enableOffline !== 'undefined' && typeof enableOffline === 'boolean') {
+        _enableOfflineSave = enableOffline;
       }
 
-      if (!tags || typeof tags !== 'string') {
-        // To avoid 0
-        tags = null;
-      }
-
-      processEvent(message, tags, data);
+      return atatus;
     }
   };
 
   /* internals */
 
+  function truncateURL(url){
+      // truncate after fourth /, or 24 characters, whichever is shorter
+      // /api/1/diagrams/xyz/server becomes
+      // /api/1/diagrams/...
+      var path = url.split('//')[1];
+      var queryStart = path.indexOf('?');
+      var sanitizedPath = path.toString().substring(0, queryStart);
+      var truncated_parts = sanitizedPath.split('/').slice(0, 4).join('/');
+      var truncated_length = sanitizedPath.substring(0, 48);
+      var truncated = truncated_parts.length < truncated_length.length?
+                      truncated_parts : truncated_length;
+      if (truncated !== sanitizedPath) {
+          truncated += '..';
+      }
+      return truncated;
+  }
+
   function processJQueryAjaxError(event, jqXHR, ajaxSettings, thrownError) {
+    var message = 'AJAX Error: ' +
+        (jqXHR.statusText || 'unknown') +' '+
+        (ajaxSettings.type || 'unknown') + ' '+
+        (truncateURL(ajaxSettings.url) || 'unknown');
     atatus.send(thrownError || event.type, {
       status: jqXHR.status,
       statusText: jqXHR.statusText,
       type: ajaxSettings.type,
       url: ajaxSettings.url,
+      ajaxErrorMessage: message,
       contentType: ajaxSettings.contentType,
       data: ajaxSettings.data ? ajaxSettings.data.slice(0, 10240) : undefined });
   }
 
-  function log(message) {
+  function log(message, data) {
     if (window.console && window.console.log && _debugMode) {
       window.console.log(message);
+
+      if (data) {
+        window.console.log(data);
+      }
     }
   }
 
@@ -150,6 +182,13 @@
     return o3;
   }
 
+  function mergeArray(t0, t1) {
+    if (t1 != null) {
+      return t0.concat(t1);
+    }
+    return t0;
+  }
+
   function forEach(set, func) {
     for (var i = 0; i < set.length; i++) {
       func.call(null, i, set[i]);
@@ -165,6 +204,10 @@
     return true;
   }
 
+  function getRandomInt() {
+    return Math.floor(Math.random() * 9007199254740993);
+  }
+
   function getViewPort() {
     var e = document.documentElement,
     g = document.getElementsByTagName('body')[0],
@@ -173,9 +216,33 @@
     return { width: x, height: y };
   }
 
+  function offlineSave (data) {
+    var dateTime = new Date().toJSON();
+
+    try {
+      var key = 'raygunjs=' + dateTime + '=' + getRandomInt();
+
+      if (typeof localStorage[key] === 'undefined') {
+        localStorage[key] = data;
+      }
+    } catch (e) {
+      log('AtatusJS: LocalStorage full, cannot save exception');
+    }
+  }
+
+  function sendSavedErrors() {
+    for (var key in localStorage) {
+      if (key.substring(0, 9) === 'raygunjs=') {
+        sendToAtatus(JSON.parse(localStorage[key]));
+
+        localStorage.removeItem(key);
+      }
+    }
+  }
+
   function processUnhandledException(stackTrace, options) {
     var stack = [],
-        message;
+        qs = {};
 
     // Create stack trace array
     if (stackTrace.stack && stackTrace.stack.length) {
@@ -189,37 +256,43 @@
       });
     }
 
-    // Create search query object
-    // var qs = {};
-    // if (window.location.search && window.location.search.length > 1) {
-    //   forEach(window.location.search.substring(1).split('&'), function (i, segment) {
-    //     var parts = segment.split('=');
-    //     if (parts && parts.length === 2) {
-    //       qs[decodeURIComponent(parts[0])] = parts[1];
-    //     }
-    //   });
-    // }
-
-    // Remove 'Uncaught ' from prefix. It happen only in Chrome, Opera
-    // Firefox, Safari does not add this prefix.
-    if (stackTrace.message && stackTrace.message.indexOf('Uncaught ') === 0) {
-        message = stackTrace.message.substring(9);
-    } else {
-        message = stackTrace.message || 'Script error';
+    //Create search query object
+    if (window.location.search && window.location.search.length > 1) {
+      forEach(window.location.search.substring(1).split('&'), function (i, segment) {
+        var parts = segment.split('=');
+        if (parts && parts.length === 2) {
+          qs[decodeURIComponent(parts[0])] = parts[1];
+        }
+      });
     }
 
-    if (isEmpty(options)) {
-      options = _customData;
+    if (options === undefined) {
+      options = {};
+    }
+
+    if (isEmpty(options.customData)) {
+      options.customData = _customData;
+    }
+
+    if (isEmpty(options.tags)) {
+      options.tags = _tags;
     }
 
     var screen = window.screen || { width: getViewPort().width, height: getViewPort().height, colorDepth: 8 };
+
+    // Remove 'Uncaught ' from prefix. It happen only in Chrome, Opera
+    // Firefox, Safari does not add this prefix.
+    var custom_message = (options.customData && options.customData.ajaxErrorMessage) || stackTrace.message;
+    if (custom_message && custom_message.indexOf('Uncaught ') === 0) {
+        custom_message = stackTrace.message.substring(9);
+    }
 
     var payload = {
       'occurred_on': new Date(),
       'details': {
         'error': {
           'classname': stackTrace.name,
-          'message': message,
+          'message': custom_message || options.status || 'Script error',
           'stacktrace': stack
         },
         'environment': {
@@ -238,53 +311,21 @@
         },
         'client': {
           'name': 'atatus-js',
-          'version': '1.0.0'
+          'version': '1.8.1'
         },
-        'user_custom_data': options,
-        'version': _version || 'Not supplied'
+        'user_custom_data': options.customData,
+        'tags': options.tags,
+        'Version': _version || 'Not supplied'
       }
     };
 
     if (_user) {
-      payload.details.user = _user;
+      payload.Details.User = _user;
     }
-    sendToAtatus(payload, 'exception');
+    sendToAtatus(payload);
   }
 
-  function processEvent(message, tags, data) {
-    // Add user and version to tags
-    if (_user) {
-      if (tags) {
-        tags += ',' + _user;
-      } else {
-        tags = _user;
-      }
-    }
-    if (_version) {
-      if (tags) {
-        tags += ',' + _version;
-      } else {
-        tags = _version;
-      }
-    }
-
-    var payload = {
-      'message': message,
-      'tags': tags,
-      'data': data,
-      'environment': {
-        'user_agent': _userAgent,
-        'url': document.location.href,
-        'referrer': document.referrer,
-        'host': document.domain,
-        'query_string': window.location.search
-      },
-      'occurred_on': new Date()
-    };
-    sendToAtatus(payload, 'log');
-  }
-
-  function sendToAtatus(data, type) {
+  function sendToAtatus(data) {
     // Check for allowed domain
     if (_allowedDomains &&
         _allowedDomains.indexOf(location.host) === -1) {
@@ -294,8 +335,8 @@
     if (!isApiKeyConfigured()) {
       return;
     }
-    log('Sending data to Atatus:', data);
-    var url = 'https://www.atatus.com/api/entries/' + type + '?apikey=' + encodeURIComponent(_atatusApiKey);
+    log('Sending exception data to Atatus:', data);
+    var url = _atatusApiUrl + '/api/entries?apikey=' + encodeURIComponent(_atatusApiKey);
     makeCorsRequest(url, JSON.stringify(data));
   }
 
@@ -307,6 +348,7 @@
     if ("withCredentials" in xhr) {
       // XHR for Chrome/Firefox/Opera/Safari.
       xhr.open(method, url, true);
+
     } else if (window.XDomainRequest) {
       // XDomainRequest for IE.
       if (_allowInsecureSubmissions) {
@@ -315,23 +357,56 @@
         // on http
         url = url.slice(6);
       }
+
       xhr = new window.XDomainRequest();
       xhr.open(method, url);
     }
 
-    xhr.onload = function () {
-      log('logged error to Atatus');
-    };
-    xhr.onerror = function () {
-      log('failed to log error to Atatus');
-    };
+    xhr.timeout = 10000;
 
     return xhr;
   }
 
   // Make the actual CORS request.
-  function makeCorsRequest(url, data) {
-    var xhr = createCORSRequest('POST', url);
+  function makePostCorsRequest(url, data) {
+    var xhr = createCORSRequest('POST', url, data);
+
+    if ('withCredentials' in xhr) {
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) {
+          return;
+        }
+
+        if (xhr.status === 202) {
+          sendSavedErrors();
+        } else if (_enableOfflineSave && xhr.status !== 403 && xhr.status !== 400) {
+          offlineSave(data);
+        }
+      };
+
+      xhr.onload = function () {
+        log('logged error to Atatus');
+      };
+
+    } else if (window.XDomainRequest) {
+      xhr.ontimeout = function () {
+        if (_enableOfflineSave) {
+          log('Atatus: saved error locally');
+          offlineSave(data);
+        }
+      };
+
+      xhr.onload = function () {
+        log('logged error to Atatus');
+        sendSavedErrors();
+      };
+    }
+
+    xhr.onerror = function () {
+      log('failed to log error to Atatus');
+    };
+
     if (!xhr) {
       log('CORS not supported');
       return;
